d65d0973194d45d28a96843dd8c651ac6cf6a59f
Nauval <sigantengnoval@gmail.com>
2025-08-22 15:28:06 +0700

    :mag: Move metadata to layout (#42)

--- app/page.jsx
+++ app/page.jsx
@@ -51,15 +51,6 @@ import GithubIcon from "../src/assets/images/github.svg";
 // Import Stackoverflow images
 import StackoverflowIcon from "../src/assets/images/stackoverflow.png";
 
-export const metadata = {
-  robots: { index: true, follow: false },
-  title: "gopelkujo",
-  description: "my professional journey.",
-  verification: {
-    google: '6drGFjAogw6sco26TtJog-67aejfpo1VFgB68oz_gv4',
-  },
-};
-
 export default function Home() {
   const greetingText = "Hello, nice to meet you. I am Gopel!";
 
