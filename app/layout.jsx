35c292b41dcb154ebf85ebff09b425dbed705ec1
Nauval <sigantengnoval@gmail.com>
2025-08-26 13:20:02 +0700

    :bug: Fix resume broken link (#45)

--- app/layout.jsx
+++ app/layout.jsx
@@ -79,7 +79,7 @@ export default function RootLayout({ children }) {
           </Link>
           <Link
             className={styles.menuResumeButton}
-            href="https://drive.google.com/file/d/1vvnZ_9ryFLeTnEPwUZXWXbHpakjNcXAF/view?usp=sharing"
+            href="https://drive.google.com/file/d/18hrUG2XIj779LYCUDT-jYqg-ix97S9NQ/view?usp=share_link"
             target="_blank"
           >
             Resume
